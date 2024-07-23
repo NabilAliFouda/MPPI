50df98ca7f2e290aedbf2f0f1633d69fb6425c41
NabilAliFouda <nabilfouda2004@gmail.com>
2024-07-23 17:30:20 +0300

    Added publishers, subscribers and initial cost function

--- MPPI.py
+++ MPPI.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import rospy
 from nav_msgs.msg import Odometry, Path
+from std_msgs.msg import Float64
+from sensor_msgs.msg import PointCloud
+from tf.transformations import euler_from_quaternion
 import numpy as np
 import math
 #state is [x,y,heading]
@@ -11,18 +14,36 @@ import math
 
 #MPPI paramters
 DT=0.1
-HOR=4
-SAMPLES=2000
+HOR=20
+SAMPLES=10
 LAMBDA=1
 ###
 #Vehilce Parameters
 L=1
 MAXSTEER=30
 MAXVEL=10
-##
+## global variables
 init_state=np.zeros(3)
 nom_U=np.zeros((HOR,2))
-#path=
+obstacles=[]
+#callbacks for coppelia
+def odom_callback(state:Odometry):
+    global init_state
+    init_state[0]=state.pose.pose.position.x
+    init_state[1]=state.pose.pose.position.y
+    ori_w = state.pose.pose.orientation.w
+    ori_x = state.pose.pose.orientation.x
+    ori_y = state.pose.pose.orientation.y
+    ori_z = state.pose.pose.orientation.z
+    roll, pitch, yaw = euler_from_quaternion([ori_x, ori_y, ori_z, ori_w])
+    yaw += (math.pi / 2.0) ##for interfacing with Coppelia
+    init_state[2]=yaw
+def obstacle_callback(msg:PointCloud):
+    global obstacles
+    obstacles=msg.points
+
+##
+##MPPI Functions
 def forwardModel(x0, u):
     beta=u[1]/2*(np.pi/180)
     x1=x0
@@ -36,11 +57,18 @@ def calculateCost(Actions):
         states[i+1]=forwardModel(states[i],Actions[i])
     cost = 0
     for state in states:
-        ##to be edited later
-        stateCost=state[1]-state[0]
+        for obs in obstacles:
+            if math.hypot(state[0]-obs[0],state[1]-obs[1])<obs[2]:
+                return np.inf
+    cost = math.hypot(states[HOR+1,0]-init_state[0],states[HOR+1,1]-init_state[1])
+    ##to be edited later
+    '''
+        stateCost=(state[1]-state[0])+5
         cost+=stateCost
         ###
+        '''
     return cost
+
 def generateControlActions():
     perturbations=np.random.rand(HOR,2)-np.full((HOR,2),0.5)
     scale=np.array([[2*MAXVEL,0],[0,2*MAXSTEER]])
@@ -53,7 +81,9 @@ def FindOptimalControlActions():
     for i in range(SAMPLES):
         Dus[i]=generateControlActions()
         costs[i]=calculateCost(nom_U+Dus[i])
+    #print("costs = "+str(costs))
     weights=np.exp(-(1/LAMBDA)*costs)
+    #print("weights = "+str(weights))
     Du=np.einsum("i,ijk->jk",weights,Dus)*(1/(weights.sum(0)))
     nom_U=nom_U+Du
     return nom_U
@@ -62,7 +92,14 @@ def FindOptimalControlActions():
         Du_2+=Dus[i]*weights[i]
     print("Final perturbation = "+str(Du))
     print("Final perturbation 2 = "+str(Du_2))'''
-
+###
+##Subscribers and publishers for Coppelia
+brakes_pub = rospy.Publisher('/brakes', Float64, queue_size=10)
+state_sub=rospy.Subscriber('/odom',Odometry,callback=odom_callback,queue_size=10)
+throttle_pub=rospy.Publisher('/cmd_vel',Float64,queue_size=10)
+steer_pub=rospy.Publisher('/SteeringAngle',Float64,queue_size=10)
+obstacle_sub=rospy.Subscriber("/boundary_points", PointCloud, callback=obstacle_callback)
+##
 
 if __name__ == '__main__' :
     rospy.init_node("sim")
